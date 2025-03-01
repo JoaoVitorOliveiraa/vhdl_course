d5c665dab99191438acb3e732085279736270d7c
JoaoVitorOliveiraa <jvoliveira30@gmail.com>
2025-03-01 15:03:14 -0300

    Exemplo que um somador de 4 bits.

---
+++ somador_quatro_bits.vhd
@@ -0,0 +1,19 @@
+-- Exemplo de um Somador de Quatro Bits.
+-- Autor: João Vitor dos Santos Oliveira
+-- Data: 01/03/2025
+
+entity somador_quatro_bits is
+
+    port(
+        a, b: in integer range 0 to 15;
+        z: out integer range 0 to 15
+    );
+
+end entity somador_quatro_bits;
+
+architecture main of somador_quatro_bits is
+begin
+
+    z <= a + b:
+
+end architecture main;
