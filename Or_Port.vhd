f61997e9c20a803dcf54e2a9f799bcedd5b54859
JoaoVitorOliveiraa <jvoliveira30@gmail.com>
2025-03-30 13:26:54 -0300

    Porta Or.

---
+++ Or_Port.vhd
@@ -0,0 +1,15 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity Or_Port is
+    Port ( A : in  STD_LOGIC;
+           B : in  STD_LOGIC;
+           S : out  STD_LOGIC);
+end Or_Port;
+
+architecture Behavioral of Or_Port is
+
+begin
+    S <= A or B;
+    
+end Behavioral;
