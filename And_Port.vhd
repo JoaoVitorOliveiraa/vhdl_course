b01c4beafc1d48015130f4f805cb735677c4d54a
JoaoVitorOliveiraa <jvoliveira30@gmail.com>
2025-03-30 13:26:41 -0300

    Porta And.

---
+++ And_Port.vhd
@@ -0,0 +1,15 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity And_Port is
+    Port ( A : in  STD_LOGIC;
+           B : in  STD_LOGIC;
+           S : out  STD_LOGIC);
+end And_Port;
+
+architecture Behavioral of And_Port is
+
+begin 
+    S <= A and B;
+    
+end Behavioral;
